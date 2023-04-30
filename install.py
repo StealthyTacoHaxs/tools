4969df27c2fd68e9f6611866ef7429c8ae6462ac
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2023-04-30 15:00:18 -0700

    add remote package installer

--- install.py
+++ install.py
@@ -4,22 +4,62 @@ import os
 import sys
 import shutil
 import subprocess
+import ruamel.yaml as yaml # pip3 install ruamel.yaml
 import argparse
 
-HOME = os.path.expanduser("~")
+parser = argparse.ArgumentParser(description="Install the dotfiles")
+parser.add_argument(
+    "-p",
+    "--packages",
+    help="Install packages",
+    action="store_true")
+parser.add_argument(
+    "-b",
+    "--bashrc",
+    help="Install bashrc",
+    action="store_true")
+parser.add_argument(
+    "-n",
+    "--dry-run",
+    help="Dry run",
+    action="store_true"
+)
+args = parser.parse_args()
+
+if args.dry_run:
+    env_dir = os.path.join(os.getcwd(), ".env")
+    if not os.path.exists(env_dir):
+        os.makedirs(env_dir, exist_ok=True)
+    HOME = env_dir
+else:
+    HOME = os.path.expanduser("~")
+
 PATH = os.path.dirname(os.path.realpath(__file__))
 PYTHON_SITE_PACKAGES = subprocess.check_output(
     ["python3", "-m", "site", "--user-site"]).decode("utf-8").strip()
 
-PYTHON_PACKAGES = [
-    "dirsearch"
-]
+PACKAGES = os.listdir(PATH + "/submodules")
 
-def install_python_packages():
+def parse_remote_packages_list():
+    with open(PATH + "/remote_packages.yml", "r") as f:
+        remote_packages = yaml.load(f, Loader=yaml.Loader)
+    return remote_packages
+
+def install_packages():
     print("Installing python packages...")
-    for package in PYTHON_PACKAGES:
+    for package in PACKAGES:
+        if not os.path.exists(PATH + "/submodules/" + package + "/setup.py"):
+            continue
+        print("Installing " + package + "...")
         subprocess.call(["pip3", "install", "-e", "submodules/" + package])
-    # add python packages to path
+    print("Installing remote packages...")
+    remote_packages = parse_remote_packages_list()
+    for package in remote_packages['apt']:
+        print("Installing " + package + "...")
+        subprocess.call(["sudo", "apt", "install", "-y", package])
+    for package in remote_packages['pip']:
+        print("Installing " + package + "...")
+        subprocess.call(["pip3", "install", package])
     shell = get_shell()
     if shell == "bash":
         with open(HOME + "/.bashrc", "a") as f:
@@ -27,7 +67,7 @@ def install_python_packages():
             f.write("export PATH=$PATH:" + PYTHON_SITE_PACKAGES + "\n")
     elif shell == "fish":
         if not os.path.exists(HOME + "/.config/fish"):
-            os.makedirs(HOME + "/.config/fish")
+            os.makedirs(HOME + "/.config/fish", exist_ok=True)
         with open(HOME + "/.config/fish/config.fish", "a") as f:
             f.write("\n# add python packages to path\n")
             f.write("set -x PATH $PATH " + PYTHON_SITE_PACKAGES + "\n")
@@ -52,20 +92,15 @@ def get_shell():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Install the dotfiles")
-    parser.add_argument(
-        "-p",
-        "--python",
-        help="Install python packages",
-        action="store_true")
-    parser.add_argument(
-        "-b",
-        "--bashrc",
-        help="Install bashrc",
-        action="store_true")
-    args = parser.parse_args()
-
-    if args.python:
-        install_python_packages()
+    if args.packages:
+        install_packages()
+        exit(0)
     if args.bashrc:
         pass
+    if args._get_args() == []:
+        print("No arguments given.")
+        prompt = input("Would you like to install packages? (y/n) ")
+        if prompt == "y":
+            args.packages = True
+        else:
+            exit(1)
